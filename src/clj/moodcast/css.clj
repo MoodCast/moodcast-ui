740abbf359916121511d1083966aeb6b30a02849
Markku Rontu <markku.rontu@iki.fi>
2016-04-23 11:45:12 +0300

    style: sharkman normal style tuning

--- src/clj/moodcast/css.clj
+++ src/clj/moodcast/css.clj
@@ -12,8 +12,10 @@
    {:width (px 200)
     :height (px 320)}
    [:.part.sharkman.mask {:left (px 48)
-                                   :top (px -370)}]
+                          :top (px -370)}]
+   [:.part.sharkman.mask.normal {:left (px 58)
+                                 :top (px -370)}]
    [:.part {:position :relative
-                     :left (px 0)
-                     :top (px 0)
-                     :width (em 2)}]])
+            :left (px 0)
+            :top (px 0)
+            :width (em 2)}]])
